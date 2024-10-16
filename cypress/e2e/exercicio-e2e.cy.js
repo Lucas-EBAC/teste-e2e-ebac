8e6ac378859a83d8b95a7fa8e4ac08ac5980a692
Lucas <lucastsunoda3@gmail.com>
2024-10-16 14:47:43 -0300

    Exercicio

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,4 +1,7 @@
 /// <reference types="cypress" />
+import produtosPage from "../support/page_objects/produtos.page";
+import {fakerPT_BR } from '@faker-js/faker';
+
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
   /*  Como cliente 
@@ -10,13 +13,33 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
       E validando minha compra ao final */
 
   beforeEach(() => {
-      cy.visit('/')
+      cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
   });
 
   it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-      //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
-      
-  });
-
-
-})
+    cy.fixture('produtos').then(dados => {
+        produtosPage.buscarProduto(dados[0].nomeProduto)
+        produtosPage.addProdutoCarrinho(
+            dados[0].tamanho, 
+            dados[0].cor, 
+            dados[0].quantidade)
+        cy.get('.woocommerce-message').should('contain' , dados[0].nomeProduto)
+        cy.get('#cart > .dropdown-toggle').click()
+        cy.get('.buttons')
+        cy.get('.checkout').click()
+        cy.get('#billing_first_name').type(fakerPT_BR.person.firstName())
+        cy.get('#billing_last_name').type(fakerPT_BR.person.lastName())
+        cy.get('#billing_address_1').type(fakerPT_BR.location.streetAddress())
+        cy.get('#billing_city').type(fakerPT_BR.location.city())
+        cy.get('#billing_postcode').type(fakerPT_BR.location.zipCode())
+        cy.get('#billing_phone').type(fakerPT_BR.phone.number())
+        cy.get('#billing_email').type(fakerPT_BR.internet.email())
+        cy.get('#createaccount').click()
+        cy.get('#account_password').type(fakerPT_BR.internet.password())
+        cy.get('#terms').click()
+        cy.get('#place_order').click()
+       });
+       })
+   
+   
+   })
