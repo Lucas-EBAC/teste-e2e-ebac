aa2e5727f48893a4d91e5b602ebced95643faf32
Lucas <lucastsunoda3@gmail.com>
2024-10-17 13:52:17 -0300

    Exercicio

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -38,6 +38,7 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         cy.get('#account_password').type(fakerPT_BR.internet.password())
         cy.get('#terms').click()
         cy.get('#place_order').click()
+        cy.get('.woocommerce-notice').should('contain' , 'Obrigado. Seu pedido foi recebido.')
        });
        })
    
